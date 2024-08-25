856f6bc449de53c4aae2fea83f5f81d9c812c8cd
Amirmohseni87 <amirmohseni87@yahoo.com>
2024-08-25 18:25:23 +0200

    Upgraded the versions, added length preference and better transcriptions

--- telegram/Summarizer.py
+++ telegram/Summarizer.py
@@ -1,33 +1,38 @@
-from pytube import YouTube
 import os
-import subprocess
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound, NoTranscriptAvailable
 from openai import OpenAI
 
+
 def get_youtube_transcript(url):
-   # Extract the video ID from the URL considering different formats
+    # Extract the video ID from the URL considering different formats
     if "youtube.com" in url:
         video_id = url.split("v=")[1].split("&")[0]
     elif "youtu.be" in url:
         video_id = url.split("/")[-1]
     else:
         raise ValueError("Invalid YouTube URL. Please provide a valid YouTube video URL.")
-    
+
     try:
         # Attempt to get the manually created transcript first
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
     except (TranscriptsDisabled, NoTranscriptFound, NoTranscriptAvailable):
         # If manual transcript is not available, get the auto-generated one
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'], preserve_formatting=True)
-    
+
     # Convert transcript to a string
     transcript_str = '\n'.join([item['text'] for item in transcript])
     return transcript_str
 
-def summarize_text_gpt(transcript):
-    system_msg = 'You are a model that receives a transcription of a YouTube video. Your task is to correct any words that may be incorrect based on the context, and transform it into a well-structured summary of the entire video. Your summary should highlight important details and provide additional context when necessary. Aim to be detailed, particularly when addressing non-trivial aspects of the content. The summary should encompass at least 20-30% of the original text length.'
-    client = OpenAI(api_key=userdata.get('OPENAI_API_KEY'))
-    token_limit = max(len(transcript) // 2, 4096)
+
+def summarize_text_gpt(transcript, max_tokens=128):
+    system_msg = 'You are a model that receives a transcription of a YouTube video. Your task is to correct any words ' \
+                 'that may be incorrect based on the context, and transform it into a well-structured summary of the ' \
+                 'entire video. Your summary should highlight important details and provide additional context when ' \
+                 'necessary. Aim to be detailed, particularly when addressing non-trivial aspects of the content. The ' \
+                 'summary should encompass at least 20-30% of the original text length.'
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+    max_tokens = min(max_tokens, len(transcript))
 
     try:
         response = client.chat.completions.create(
@@ -36,26 +41,31 @@ def summarize_text_gpt(transcript):
                 {"role": "system", "content": system_msg},
                 {"role": "user", "content": transcript},
             ],
-            max_tokens=token_limit,
+            max_tokens=max_tokens,
         )
         return response.choices[0].message.content
     except Exception as e:
         raise RuntimeError(f"Encountered error when summarizing the transcript")
 
-def summarize_yt_video(url):
+
+def summarize_yt_video(url, summary_type='medium'):
     try:
         transcript = get_youtube_transcript(url)
-        summary = summarize_text_gpt(transcript)
+        length_dict = {'short': 128, 'medium': 256, 'long': 512, 'extra_long': 2048}
+        max_tokens = length_dict[summary_type]
+
+        summary = summarize_text_gpt(transcript, max_tokens=max_tokens)
         return summary
     except Exception as e:
         return f"Error processing YouTube URL: {e}"
 
+
 if __name__ == '__main__':
     print("This file contains functions for YouTube video summarization.")
-    
+
     # Example URL for testing
     test_url = "https://www.youtube.com/watch?v=81JgczyzXy8"
-    
+
     # Test the process_youtube_url function
     try:
         summary = summarize_yt_video(test_url)
--- telegram/UserHandler.py
+++ telegram/UserHandler.py
@@ -2,7 +2,7 @@ import os
 import telebot
 from dotenv import load_dotenv
 from Summarizer import summarize_yt_video
-import openai
+
 
 def escape_markdown_v2(text):
     """
@@ -10,6 +10,7 @@ def escape_markdown_v2(text):
     """
     escape_chars = r'_*[]()~`>#+-=|{}.!'
     return ''.join(['\\' + char if char in escape_chars else char for char in text])
+
 
 def main():
     try:
@@ -25,22 +26,23 @@ def main():
             itembtn1 = telebot.types.KeyboardButton(text='/summarize')
             itembtn2 = telebot.types.KeyboardButton(text='/help')
             markup.add(itembtn1, itembtn2)
-            bot.send_message(message.chat.id, welcome_message, parse_mode="Markdown")
+            bot.send_message(message.chat.id, welcome_message, parse_mode="Markdown", reply_markup=markup)
 
         @bot.message_handler(commands=['help'])
         def help(message):
-            help_text = "This bot can summarize the content of a YouTube video.\nTo get started, use the /summarize " \
-                        "command. You will be prompted to enter the URL of the YouTube video you would like to summarize. " \
-                        "The bot will then process the video and provide you with a summary of its content.\nEnjoy!"
+            help_text = ("This bot can summarize the content of a YouTube video.\n"
+                         "To get started, use the /summarize command. You will be prompted to enter the URL of the "
+                         "YouTube video"
+                         "and choose the summary length: short, medium, or long.\nEnjoy!")
             bot.send_message(message.chat.id, help_text, parse_mode="Markdown")
 
-        def url_handler(message):
+        def url_handler(message, summary_type):
             url = message.text
             print(f"Received YouTube URL: {url}")
 
             try:
-                summary = summarize_yt_video(url)
-                summary = escape_markdown_v2(summary)  # Escape special characters for MarkdownV2
+                summary = summarize_yt_video(url, summary_type)
+                summary = escape_markdown_v2(summary)
             except Exception as e:
                 summary = f"Error processing YouTube URL: {e}\nPlease try again."
 
@@ -48,14 +50,27 @@ def main():
 
         @bot.message_handler(commands=['summarize'])
         def message_handler(message):
+            markup = telebot.types.InlineKeyboardMarkup(row_width=3)
+            short_btn = telebot.types.InlineKeyboardButton(text='Short', callback_data='short')
+            medium_btn = telebot.types.InlineKeyboardButton(text='Medium', callback_data='medium')
+            long_btn = telebot.types.InlineKeyboardButton(text='Long', callback_data='long')
+            extra_long_btn = telebot.types.InlineKeyboardButton(text='Extra Long', callback_data='extra_long')
+            markup.add(short_btn, medium_btn, long_btn)
+            bot.send_message(message.chat.id, "Choose the summary length:", reply_markup=markup)
+
+        @bot.callback_query_handler(func=lambda call: call.data in ['short', 'medium', 'long', 'extra_long'])
+        def callback_query_handler(call):
             text = "Please enter the URL of the YouTube video you would like to summarize."
-            sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-            bot.register_next_step_handler(sent_msg, url_handler)
+            summary_type = call.data
+
+            sent_msg = bot.send_message(call.message.chat.id, text, parse_mode="Markdown")
+            bot.register_next_step_handler(sent_msg, lambda message: url_handler(message, summary_type))
 
         bot.infinity_polling()
 
     except Exception as e:
         print(f"Error in main function: {e}")
+
 
 if __name__ == '__main__':
     main()
